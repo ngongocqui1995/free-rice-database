40364d7cfdf5a85740868ed8267261f24169ad0d
qui <tranphanchau10@gmail.com>
2022-01-14 14:36:37 +0700

    update

--- src/events/cronjob/cronjob.module.ts
+++ src/events/cronjob/cronjob.module.ts
@@ -7,9 +7,10 @@ import { GlobalService } from '../../common/global.service';
 import { Vocabulary } from '../../modules/vocabulary/entities/vocabulary.entity';
 import { VocabularyService } from '../../modules/vocabulary/vocabulary.service';
 import { CronjobService } from './cronjob.service';
+import { Account } from '../../modules/account/entities/account.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Vocabulary])],
+  imports: [TypeOrmModule.forFeature([Vocabulary, Account])],
   controllers: [],
   providers: [CronjobService, BaseService, GlobalService, BaseController, VocabularyService, AccountService]
 })
